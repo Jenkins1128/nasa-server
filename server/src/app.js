fe82c176646189e4cb3c4eeca729c8f08cbd3d78
Isaiah Jenkins <ij27900@gmail.com>
2023-02-25 10:41:04 -0800

    Added Morgan middleware for logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -7,8 +9,10 @@ const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000'
-}))
+}));
+app.use(morgan('combined'));
 app.use(express.json());
+
 app.use(planetsRouter);
 
 module.exports = app;
