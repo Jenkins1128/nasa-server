333a7340140d48d319477e04133c5545c0436539
Isaiah Jenkins <ij27900@gmail.com>
2023-02-25 15:51:55 -0800

    Added GET launches & POST new launch

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
-
+const launchesRouter = require('./routes/launches/launches.router');
 const app = express();
 
 app.use(cors({
@@ -13,6 +13,7 @@ app.use(cors({
 app.use(morgan('combined'));
 app.use(express.json());
 
-app.use(planetsRouter);
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
 
 module.exports = app;
