22fb47db5c153ccfffc65a8e50379296af6d48ba
Isaiah Jenkins <ij27900@gmail.com>
2023-02-27 09:50:35 -0800

    Added funcs for new launch & abort launch

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-442 b',
     customer: ['ZTM', 'NASA'],
     upcoming: true,
     success: true
@@ -15,18 +15,34 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function existsLaunchWithId(launchId) {
+    return launches.has(launchId);
+}
+
 function getAllLaunches() {
     return Array.from(launches.values());
 }
 
 function addNewLaunch(launch) {
     latestFlightNumber++;
-    launches.set(launch.flightNumber, Object.assign(launch, {
-        flightNumber: latestFlightNumber
+    launches.set(latestFlightNumber, Object.assign(launch, {
+        flightNumber: latestFlightNumber,
+        upcoming: true,
+        success: true,
+        customer: ['Isaiah', 'Jason'],
     }));
+}
+
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted;
 }
 
 module.exports = {
     getAllLaunches,
-    addNewLaunch
+    addNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById
 }
